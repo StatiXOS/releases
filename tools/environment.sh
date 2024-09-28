0660a43a826469fb86c7b32549d7b40434dfce20
ant <ant@national.shitposting.agency>
2024-09-28 19:23:29 +0200

    chore: Provide curly brackets to `GITHUB_ENV`

--- tools/environment.sh
+++ tools/environment.sh
@@ -4,40 +4,40 @@
 # Select device codename for ${TARGET_DEVICE}
 case "${DEVICE}" in
   "Xiaomi POCO M6 Pro 5G / Redmi 12 5G / Redmi Note 12R")
-    echo "DEVICE=sky" >> "$GITHUB_ENV"
+    echo "DEVICE=sky" >> "${GITHUB_ENV}"
   ;;
   "Xiaomi SM7125 family")
-    echo "DEVICE=miatoll" >> "$GITHUB_ENV"
+    echo "DEVICE=miatoll" >> "${GITHUB_ENV}"
   ;;
 esac
 
 # Select package type for ${VARIANT}
 case "${BUILD_VARIANT}" in
   "Stable (user)")
-    echo "BUILD_VARIANT=user" >> "$GITHUB_ENV"
+    echo "BUILD_VARIANT=user" >> "${GITHUB_ENV}"
   ;;
   "Debugging (userdebug)")
-    echo "BUILD_VARIANT=userdebug" >> "$GITHUB_ENV"
+    echo "BUILD_VARIANT=userdebug" >> "${GITHUB_ENV}"
   ;;
   "Development (eng)")
-    echo "BUILD_VARIANT=eng" >> "$GITHUB_ENV"
+    echo "BUILD_VARIANT=eng" >> "${GITHUB_ENV}"
   ;;
 esac
 
 # Select package type for ${PKG}
 case "${PACKAGE}" in
   "Recovery-flashable (otapackage)")
-    echo "PACKAGE=otapackage" >> "$GITHUB_ENV"
+    echo "PACKAGE=otapackage" >> "${GITHUB_ENV}"
   ;;
   "Bootloader-flashable (updatepackage)")
-    echo "PACKAGE=updatepackage" >> "$GITHUB_ENV"
+    echo "PACKAGE=updatepackage" >> "${GITHUB_ENV}"
   ;;
 esac
 
 # Set ${CLEAN} to true for 'make clean'
 if [ -n "${CLEAN}" ]; then
-    echo "CLEAN=true" >> "$GITHUB_ENV"
+    echo "CLEAN=true" >> "${GITHUB_ENV}"
 else
     # Otherwise, set it to 'false' by default
-    echo "CLEAN=false" >> "$GITHUB_ENV"
+    echo "CLEAN=false" >> "${GITHUB_ENV}"
 fi
